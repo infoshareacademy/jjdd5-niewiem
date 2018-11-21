1616a1a0784b1974168735608ebdaab264524793
anegowska <a.negowska@wp.pl>
2018-11-21 16:34:47 +0100

    JD5NW-5-Maven

---
+++ project/src/main/java/niewiem/App.java
@@ -0,0 +1,13 @@
+package niewiem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
