852999981c07b2391c098a084c56dbdc63e88684
anegowska <a.negowska@wp.pl>
2018-11-26 00:58:03 +0100

    save and read of files

--- src/main/java/com/infoshareacademy/niewiem/FileHandler.java
+++ src/main/java/com/infoshareacademy/niewiem/FileHandler.java
@@ -1,6 +1,64 @@
 package com.infoshareacademy.niewiem;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileHandler {
     // read files with time logs for a given hall
     // read files with table data specific to a given hall
+
+    public static void main(String[] args) {
+
+        String fileName = "file.csv";
+
+        List<Integer> tableList = new ArrayList<>();
+        tableList.add(1);
+        tableList.add(2);
+        tableList.add(4);
+
+        saveFile(fileName, tableList);
+        List <String> reader = readFile(fileName);
+        System.out.println(reader);
+
+    }
+
+    public static void saveFile(String fileName, List<Integer> tableList) {
+
+        Path path = Paths.get("files", fileName);
+        List<String> out = new ArrayList<>();
+
+        for (int i = 0; i < tableList.size(); i++) {
+            String row = tableList.get(i).toString();
+
+            out.add(row);
+
+            try {
+                Files.write(path, out);
+            } catch (IOException ex) {
+                System.out.println("Can't save this file!");
+            }
+        }
+    }
+
+    public static List<String> readFile(String fileName) {
+
+        Path path = Paths.get("files", fileName);
+
+        List<String> out = new ArrayList<>();
+        try {
+            out = Files.readAllLines(path);
+        } catch (IOException ex) {
+            System.out.println("Can't find this file!");
+        }
+        return out;
+
+    }
 }
+
+
+
+
