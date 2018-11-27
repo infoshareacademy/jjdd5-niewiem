b442a34c850cf21d00f478c998e4562386e4c01c
anegowska <a.negowska@wp.pl>
2018-11-27 16:23:06 +0100

    FileHandler

--- src/main/java/com/infoshareacademy/niewiem/FileHandler.java
+++ src/main/java/com/infoshareacademy/niewiem/FileHandler.java
@@ -5,57 +5,88 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileHandler {
+
     // read files with time logs for a given hall
     // read files with table data specific to a given hall
+    private  Path path;
+    private  Path tablesPath;
+    private  Path hallsPath;
+    private  Path historyPath;
+    private  Path reservationsPath;
 
-    public static void main(String[] args) {
 
-        String fileName = "file.csv";
-
-        List<Integer> tableList = new ArrayList<>();
-        tableList.add(1);
-        tableList.add(2);
-        tableList.add(4);
-
-        saveFile(fileName, tableList);
-        List <String> reader = readFile(fileName);
-        System.out.println(reader);
-
+    public FileHandler() {
+        this.path = Paths.get("data");
+        this.tablesPath = Paths.get(path.toString(),"tables.csv");
+        this.hallsPath = Paths.get(path.toString(), "halls.csv");
+        this.historyPath = Paths.get(path.toString(), "history.csv");
+        this.reservationsPath = Paths.get(path.toString(), "reservation.csv");
     }
 
-    public static void saveFile(String fileName, List<Integer> tableList) {
 
-        Path path = Paths.get("files", fileName);
+    public void saveDataToFile(List<Table> tables, Hall hall) {
+
         List<String> out = new ArrayList<>();
 
-        for (int i = 0; i < tableList.size(); i++) {
-            String row = tableList.get(i).toString();
+        try {
+            for (Table table : tables) {
+                List<String> properties = Arrays.asList(
+                        String.valueOf(table.getTableID()),
+                        String.valueOf(table.getType()),
+                        String.valueOf(hall.getName())
+                );
 
-            out.add(row);
-
-            try {
-                Files.write(path, out);
-            } catch (IOException ex) {
-                System.out.println("Can't save this file!");
+                String singleEntry = properties.stream().collect(Collectors.joining(","));
+                out.add(singleEntry);
             }
+            Files.write(tablesPath, out);
+
+        } catch (IOException ex) {
+            System.out.println("Can't save this file!");
         }
     }
 
-    public static List<String> readFile(String fileName) {
+    public List<Table> readFromTables() {
 
-        Path path = Paths.get("files", fileName);
+        List<Table> tables = new LinkedList<>();
 
-        List<String> out = new ArrayList<>();
         try {
-            out = Files.readAllLines(path);
+            for (String line : Files.readAllLines(tablesPath)) {
+                tables.add(createTable(line.split(",")));
+            }
+
         } catch (IOException ex) {
             System.out.println("Can't find this file!");
         }
-        return out;
 
+        return tables;
+    }
+
+    public List<Table> readFromTables(String club) {
+
+        List<Table> tables = new LinkedList<>();
+
+        try {
+            for (String line : Files.readAllLines(tablesPath)) {
+                //if
+                tables.add(createTable(line.split(",")));
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Can't find this file!");
+        }
+
+        return tables;
+    }
+
+    public Table createTable(String [] splittedLine) {
+        return new Table(Integer.valueOf(splittedLine[0]), TableType.valueOf(splittedLine[1]));
     }
 }
 
