c3e4a0a43c2783a8bd2769485c509d7279cf9c59
Bert Pie <BertPieGIT@gmail.com>
2018-12-07 23:53:52 +0100

    created simple daos for pojos

--- ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/dao/HallDao.java
+++ ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/dao/HallDao.java
@@ -5,6 +5,8 @@ import com.infoshareacademy.niewiem.pojo.Hall;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
 
 @Stateless
 public class HallDao {
@@ -12,8 +14,29 @@ public class HallDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public Integer save(Hall h){
-        entityManager.persist(h);
-        return h.getId();
+    public Integer save(Hall hall){
+        entityManager.persist(hall);
+        return hall.getId();
     }
+
+    public Hall update (Hall hall){
+        return entityManager.merge(hall);
+    }
+
+    public void delete(Integer id){
+        final Hall hall = entityManager.find(Hall.class, id);
+        if(hall != null){
+            entityManager.remove(hall);
+        }
+    }
+
+    public Hall findById(Integer id){
+        return entityManager.find(Hall.class, id);
+    }
+
+    public List<Hall> findAll(){
+        final Query query = entityManager.createQuery("SELECT hall FROM Hall hall");
+        return query.getResultList();
+    }
+
 }
