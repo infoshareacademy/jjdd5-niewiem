920b22e770488f1649a06e9a5f6597d05d7d2687
BertPie <BertPieGIT@gmail.com>
2018-12-11 17:03:38 +0100

    init changes to servlets

---
+++ ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/web/TablesViewServlet.java
@@ -0,0 +1,47 @@
+package com.infoshareacademy.niewiem.web;
+
+import com.infoshareacademy.niewiem.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("tables-view")
+public class TablesViewServlet extends HttpServlet {
+    private static final String TEMPLATE_NAME = "tables-view";
+    private static final Logger LOG = LoggerFactory.getLogger(TablesViewServlet.class);
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> params = req.getParameterMap();
+        resp.addHeader("Content-Type", "text/html; charset=utf-8");
+        Map<String, Object> model = new HashMap<>();
+
+        sendModelToTemplate(resp, model);
+    }
+
+    private void sendModelToTemplate(HttpServletResponse resp, Map<String, Object> model) throws IOException {
+        Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
+
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            LOG.error("Error while processing template: " + e);
+        }
+    }
+
+}
