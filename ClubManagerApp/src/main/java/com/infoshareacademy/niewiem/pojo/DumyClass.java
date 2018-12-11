d18998f199b9a29d5b6645a82e32966bc913c39a
Michal Nowakowski <michal@nowakowski.me.uk>
2018-12-11 14:47:58 +0100

    another bad bad code

--- ClubManagerApp/src/main/java/com/infoshareacademy/niewiem/pojo/DumyClass.java
+++ ClubManagerApp/src/main/java/com/infoshareacademy/niewiem/pojo/DumyClass.java
@@ -5,6 +5,10 @@ public class DumyClass {
     public void dummyMethod(Hall hall) throws Throwable {
         // should report as potential error
         int i = hall.getId() / 0;
+
+        Hall hal2 = null;
+        hal2.getName();
+
         throw new Throwable("a") {};
     }
 }
