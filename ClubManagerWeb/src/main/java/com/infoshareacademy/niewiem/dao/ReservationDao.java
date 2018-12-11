2f27085026b89bbc652d61f7af1dfd71dce77c6d
Bert Pie <BertPieGIT@gmail.com>
2018-12-11 22:57:43 +0100

    add needed queries in SQL, todo: translate to JPQL

--- ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/dao/ReservationDao.java
+++ ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/dao/ReservationDao.java
@@ -41,6 +41,17 @@ public class ReservationDao {
         return query.getResultList();
     }
 
-    //todo: find all active reservations in given hall (needs left join)
     //todo: find all active reservations for given table (needs left join)
+//    SELECT *
+//    FROM club.reservations as r
+//    LEFT JOIN club.tables as t ON r.table_id = t.id
+//    WHERE t.id = 1;
+
+    //todo: find all active reservations in given hall (needs left join)
+//    SELECT *
+//    FROM club.reservations
+//    LEFT JOIN club.tables ON table_id = club.tables.id
+//    LEFT JOIN club.halls ON club.tables.hall_id = club.halls.id
+//    WHERE halls.id = 3;
+
 }
