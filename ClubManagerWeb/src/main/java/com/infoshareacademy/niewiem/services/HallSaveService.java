6a494600c9777226f2e03cc90b197d29fc6ea5ff
anegowska <a.negowska@wp.pl>
2018-12-14 16:06:43 +0100

    save hall validation

--- ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/services/HallSaveService.java
+++ ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/services/HallSaveService.java
@@ -2,6 +2,7 @@ package com.infoshareacademy.niewiem.services;
 
 import com.infoshareacademy.niewiem.dao.HallDao;
 import com.infoshareacademy.niewiem.pojo.Hall;
+import com.infoshareacademy.niewiem.services.validators.HallValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +16,21 @@ public class HallSaveService {
     @Inject
     private HallDao hallDao;
 
+    @Inject
+    private HallValidator hallValidator;
+
     public Integer save(Hall hall){
+
+        if(hallValidator.isIdNotNull(hall)){
+            LOG.warn("Hall didn't save because id is null");
+            return -1;
+        }
+
+        if(hallValidator.isNameNotNullOrEmpty(hall)){
+            LOG.warn("Hall didn't save because name is null or empty");
+            return -1;
+        }
         // todo: validate me like you validate your French girls!
-        // id should be null, otherwise it's not save but update!
-        // name should not be null or empty
         // if image null or empty- put in the default image
         // check if image actually exist on disk
         return hallDao.save(hall);
--- ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/services/validators/HallValidator.java
+++ ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/services/validators/HallValidator.java
@@ -1,4 +1,22 @@
 package com.infoshareacademy.niewiem.services.validators;
 
+import com.infoshareacademy.niewiem.pojo.Hall;
+import com.infoshareacademy.niewiem.services.HallSaveService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HallValidator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HallSaveService.class);
+
+    public boolean isIdNotNull(Hall hall){
+        LOG.debug("");
+        return hall.getId() != null;
+    }
+
+    public boolean isNameNotNullOrEmpty(Hall hall) {
+        LOG.debug("");
+        return hall.getName() != null
+                && !hall.getName().isEmpty();
+    }
 }
