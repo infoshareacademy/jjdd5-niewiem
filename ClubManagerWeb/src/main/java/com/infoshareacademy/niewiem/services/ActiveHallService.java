73a7a9f1faceeba42a919e1dc02ba99f103952fc
Bert Pie <BertPieGIT@gmail.com>
2018-12-15 10:51:22 +0100

    merge with develop

--- ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/services/ActiveHallService.java
+++ ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/services/ActiveHallService.java
@@ -2,7 +2,6 @@ package com.infoshareacademy.niewiem.services;
 
 import com.infoshareacademy.niewiem.pojo.Hall;
 import com.infoshareacademy.niewiem.services.validators.InputValidator;
-import com.infoshareacademy.niewiem.web.ChooseHallServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +12,7 @@ import javax.servlet.http.HttpSession;
 
 @Stateless
 public class ActiveHallService {
-    private static final Logger LOG = LoggerFactory.getLogger(ChooseHallServlet.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ActiveHallService.class);
 
     @Inject
     private HallQueryService hallQueryService;
@@ -21,28 +20,26 @@ public class ActiveHallService {
     @Inject
     private InputValidator inputValidator;
 
-
-    public Hall getActiveHall(HttpSession session){
+    public Hall getActiveHall(HttpSession session) {
         Hall hall = (Hall) session.getAttribute("activeHall");
 
-        if(hall == null) {
+        if (hall == null) {
             return null;
         }
-        if(hallQueryService.doesNotExist(hall)){
+        if (hallQueryService.doesNotExist(hall)) {
             return null;
         }
         return hall;
     }
 
-    public boolean setActive(HttpSession session, String hid){
+    public boolean setActive(HttpSession session, String hid) {
         Integer hallId = inputValidator.reqIntegerValidator(hid);
         Hall hall = hallQueryService.findById(hallId);
 
-        if(hall == null) {
+        if (hall == null) {
             return false;
         }
 
-        // todo: what happens when attribute already exists?
         session.setAttribute("activeHall", hall);
         return true;
     }
