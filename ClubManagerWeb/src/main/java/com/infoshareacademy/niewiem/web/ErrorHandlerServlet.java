b89567cffeabb9ea2c27c0ab15d40be42cc383fb
tymek <tymekfeil@gmail.com>
2018-12-21 15:20:37 +0100

    error handler

---
+++ ClubManagerWeb/src/main/java/com/infoshareacademy/niewiem/web/ErrorHandlerServlet.java
@@ -0,0 +1,54 @@
+package com.infoshareacademy.niewiem.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "errorHandlerServlet",
+        urlPatterns = {"/errorHandler"},
+        loadOnStartup = 1)
+public class ErrorHandlerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+
+        Exception exception = (Exception) req.getAttribute(
+                "javax.servlet.error.exception");
+        writer.printf("exception: %s%n", exception);
+
+        Class exceptionClass = (Class) req.getAttribute(
+                "javax.servlet.error.exception_type");
+        writer.printf("exception_type: %s%n", exceptionClass);
+
+
+        Integer code = (Integer) req.getAttribute(
+                "javax.servlet.error.status_code");
+        writer.printf("status_code: 404", code);
+
+        String errorMessage = (String) req.getAttribute(
+                "javax.servlet.error.message");
+        writer.printf("message: page does not exist", errorMessage);
+
+        String requestUri = (String) req.getAttribute(
+                "javax.servlet.error.request_uri");
+
+        resp.getWriter().printf("request_uri: %s%n",
+                requestUri);
+
+        String servletName = (String) req.getAttribute(
+                "javax.servlet.error.servlet_name");
+        writer.printf("servlet_name: %s%n", servletName);
+
+    }
+}
+
+
+
